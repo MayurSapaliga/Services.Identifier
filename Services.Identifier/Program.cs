9ef725cd5106ea1a0a0c50d9eebf2670695351d3
Mayur Sapaliga <msapaliga@collabware.com>
2023-02-03 11:58:37 -0800

    Add project files.

---
+++ Services.Identifier/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Services.Identifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public string PrintIdentifier()
+        {
+            string message = "Hello from identifier service";
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
